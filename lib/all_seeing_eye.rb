ae5621b97a1858b5e01a13a2f55b9b66d595f545
Jonan Scheffler <jonanscheffler@gmail.com>
2014-11-01 21:27:15 -0700

    Add Uploader, test store_image.

--- lib/all_seeing_eye.rb
+++ lib/all_seeing_eye.rb
@@ -1,21 +1,27 @@
 require 'camera'
+require 'uploader'
 
 class AllSeeingEye
-  attr_reader :camera
+  attr_reader :camera, :uploader
 
   def initialize
     @camera = Camera.new
+    @uploader = Uploader.new
   end
 
   def spy
     image_path = @camera.capture
-    phash(image_path)
-    # push picture to s3
+    image_phash = phash(image_path)
+    store_image(image_path)
     # delete picture locally
     # tell sauron phash and url of image
   end
 
   def phash(image_path)
     Phashion::Image.new(image_path).fingerprint
+  end
+
+  def store_image(image_path)
+    @uploader.upload(image_path)
   end
 end
--- test/test_all_seeing_eye.rb
+++ test/test_all_seeing_eye.rb
@@ -3,23 +3,32 @@ require 'all_seeing_eye'
 
 class AllSeeingEyeTest < MiniTest::Unit::TestCase
   def setup
+    @fixture = 'test/fixtures/eye_of_sauron.jpg'
     @eye = AllSeeingEye.new
   end
 
   def test_spy_sends_capture_to_the_camera
+    @eye.stubs(:store_image)
     Phashion::Image.stubs(:new).returns(mock(:fingerprint))
     @eye.spy
   end
 
   def test_phash
-    phash = @eye.phash('test/fixtures/eye_of_sauron.jpg')
+    phash = @eye.phash(@fixture)
     assert_equal 8540390173105289264, phash
   end
 
   def test_spy_gets_phash_of_image
+    @eye.stubs(:store_image)
     Phashion::Image.stubs(:new)
     @eye.camera.stubs(:capture).returns('one_ring.jpg')
     @eye.expects(:phash).with('one_ring.jpg')
+    @eye.spy
+  end
+
+  def test_store_image
+    @eye.camera.stubs(:capture).returns(@fixture)
+    @eye.uploader.expects(:upload).with(@fixture)
     @eye.spy
   end
 end
